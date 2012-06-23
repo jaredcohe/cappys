c91cd38fc4b8564df4029fd950c85db4770f98c7
jec1477 <jec1477@gmail.com>
2012-06-23 12:30:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cappys::Application.config.secret_token = '6c5596730af66c7ad552d4f5ed667d9b8f978e4643d37f0dfbb5e32a6df4a4617c2dd5c0c230d9d392394b62d63a0ab553cfa8b8e8ae3b45d4afc0e026d58d15'
