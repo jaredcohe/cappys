ec77c1af760783d3c8863a6eea09cf74041753dc
jec1477 <jec1477@gmail.com>
2012-06-24 13:16:36 -0400

    stuff

--- app/models/special.rb
+++ app/models/special.rb
@@ -4,6 +4,8 @@ class Special < ActiveRecord::Base
   cattr_reader :special_types
   attr_accessible :special_type, :title, :image
   
-  has_attached_file :image #, :styles => { :small => "150x150>" } this needs imagemagick to work correctly
+  has_attached_file :image, #, :styles => { :small => "150x150>" } this needs imagemagick to work correctly
+                    :url => "/assets/specials/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/specials/:id/:style/:basename.:extension"
 
 end
